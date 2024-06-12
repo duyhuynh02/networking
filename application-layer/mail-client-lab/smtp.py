2a38ced8543aea6ec8d81fe0ec79ae5e2c273fbe
Duy Huynh <minhduyhuynh16@gmail.com>
2024-06-12 18:30:58 +0700

    update smtp optional exercise

--- application-layer/mail-client-lab/smtp.py
+++ application-layer/mail-client-lab/smtp.py
@@ -77,3 +77,13 @@ recv6 = clientSocket.recv(1024).decode()
 print(recv6)
 
 clientSocket.close()
+
+'''
+Optional Exercises	
+1. Mail servers like Google mail (address: smtp.gmail.com, port: 587) requires your client to add a 
+Transport Layer Security (TLS) or Secure Sockets Layer (SSL) for authentication and security 
+reasons, before you send MAIL FROM command. Add TLS/SSL commands to your existing ones 
+and implement your client using Google mail server at above address and port. 
+2. Your current SMTP mail client only handles sending text messages in the email body. Modify your 
+client such that it can send emails with both text and images. 
+'''
